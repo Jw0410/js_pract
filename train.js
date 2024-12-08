af80f35a27467fbc67e079f2d431c13763651aec
Jagadeeshwaran s <jagadeesh04102000@gmail.com>
2024-12-08 23:10:17 +0530

    Add files via upload

---
+++ train.js
@@ -0,0 +1,13 @@
+let num1=100;
+let num2=10;
+
+let add=num1+num2;
+let sub=num1-num2;
+let mult=num1*num2;
+let div=num1/num2;
+
+
+console.log("Addtion-"+add);
+console.log("Substraction-"+sub);
+console.log("Multiplication-"+mult);
+console.log("Division-"+div);
