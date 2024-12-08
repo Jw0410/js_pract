dcc0a689ca30269a03bc825109838bbbe0636a91
Jagadeeshwaran s <jagadeesh04102000@gmail.com>
2024-12-08 23:27:59 +0530

    Update train.js

--- train.js
+++ train.js
@@ -1,6 +1,7 @@
 let num1=100;
 let num2=10;
 
+
 let add=num1+num2;
 let sub=num1-num2;
 let mult=num1*num2;
@@ -11,3 +12,8 @@ console.log("Addtion-"+add);
 console.log("Substraction-"+sub);
 console.log("Multiplication-"+mult);
 console.log("Division-"+div);
+
+
+const number = prompt('enter the number');
+const squareRoot = Math.sqrt(number);
+console.log(`the square root of ${number} is ${squareRoot}`);
